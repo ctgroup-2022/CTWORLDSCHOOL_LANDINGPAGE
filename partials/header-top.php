6c2ca66f84e04294b74e6136e82f79b0f4b3d987
Diya <ctgroup233@gmail.com>
2025-03-25 12:51:57 +0530

    commit

---
+++ partials/header-top.php
@@ -0,0 +1,26 @@
+<div class="header-top-section" style="background-color:#fff4d5;">
+    <div class="header-top-shape">
+        <img src="assets/images/header-top-shape (1).png" alt="shape-img">
+    </div>
+    <div class="container-fluid">
+        <div class="header-top-wrapper">
+            <ul class="contact-list">
+                <li>
+                    <i class="fal fa-map-marker-alt"></i>
+                    6391 Elgin St. Celina, USA
+                </li>
+                <li>
+                    <i class="far fa-envelope"></i>
+                    <a href="mailto:info@example.com" class="link">info@example.com</a>
+                </li>
+            </ul>
+            <div class="social-icon d-flex align-items-center">
+                <span>Follow Us On:</span>
+                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fa-brands fa-linkedin-in"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ partials/header.php
@@ -0,0 +1,48 @@
+<header id="header-sticky" class="header-1" style="background-color:#fff4d5; ">
+    <div class="container-fluid">
+        <div class="mega-menu-wrapper">
+            <div class="header-main style-2">
+                <div class="header-left">
+                    <div class="logo">
+                        <a href="index.php" class="header-logo">
+                            <img src="assets/images/worldLogo.webp" alt="" style="height: 100px; width: 400px; max-width: 100%; height: auto; margin-left: 20px; margin-top: 10px; ">
+                        </a>
+                    </div>
+                  
+                </div>
+                <div class="header-right d-flex justify-content-end align-items-center">
+                    <div class="mean__menu-wrapper">
+                        <div class="main-menu">
+                            <nav id="mobile-menu">
+                                <ul>
+                                    
+                                    <li>
+                                        <a href="">
+                                            About Us</a></li>
+                                        <li><a href=""> Programs</a></li>
+                                            <li> <a href="">Activities</a></li>
+                                           <li><a href="">Testimonial</a></li>
+                                         <li><a href=""> FAQ</a></li>
+                                    
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="header-button">
+                        <a href="contact.php" class="theme-btn" style="background-color: rgb(255, 193, 7);">
+                            <span>
+                               APPLY NOW
+                                <i class="fa-solid fa-arrow-right-long"></i>
+                            </span>
+                        </a>
+                    </div>
+                    <div class="header__hamburger d-xl-none my-auto">
+                        <div class="sidebar__toggle">
+                            <i class="fas fa-bars"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
